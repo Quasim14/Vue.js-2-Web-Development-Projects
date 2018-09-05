ad86954dd2101a965b51b8500007a452d83d0ecd
quasim <nicomaxa@gmail.com>
2018-09-05 13:42:48 +0200

    Chapter02 Setting up the project
    Signed-off-by: quasim <nicomaxa@gmail.com>

---
+++ Chap02MarkdownNoteBook/script.js
@@ -0,0 +1,16 @@
+
+
+// VueJs Instance
+
+new Vue({
+    //CSS selector of the root DOM element
+
+    el: '#notebook',
+
+    data(){
+        return{
+            content : 'Ceci est une note.'
+        }
+    }
+})
+
